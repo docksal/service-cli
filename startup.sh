66c9fd7e71f2bfb95c1cada1d22abb0e361b80a4
Leonid Makarov <leonid.makarov@blinkreaction.com>
2015-06-08 11:22:11 -0400

    Update startup.sh
    Updating the condition to handle both linux and b2d .ssh folder mounts.

--- startup.sh
+++ startup.sh
@@ -4,9 +4,9 @@
 # First see if we have a mount at /.ssh
 if [ -f  /.ssh/id_rsa ]; then
   cp /.ssh/id_rsa* ~/.ssh/
-# Otherwise copy from ~/.ssh if available
-elif [ -f  ~/.ssh/id_rsa ]; then
-  cp /.ssh/id_rsa* ~/.ssh/
+# Otherwise copy from /.ssh-b2d if available
+elif [ -f  /.ssh-b2d/id_rsa ]; then
+  cp /.ssh-b2d/id_rsa* ~/.ssh/
 fi
 
 # Execute passed CMD arguments
