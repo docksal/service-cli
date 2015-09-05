657f9a14a7f4817fcffe4898be45a5f2abe2706f
Leonid Makarov <leonid.makarov@ffwagency.com>
2015-09-05 00:29:21 -0400

    Add /.ssh-b2d mount back for legacy compatibility

--- startup.sh
+++ startup.sh
@@ -5,6 +5,10 @@
 if [ -f  /.ssh/id_rsa ]; then
   cp /.ssh/id_rsa* ~/.ssh/
   chmod 600 ~/.ssh/id_rsa*
+# Or /.ssh-b2d
+elif [ -f  /.ssh-b2d/id_rsa ]; then
+  cp /.ssh-b2d/id_rsa* ~/.ssh/
+  chmod 600 ~/.ssh/id_rsa*
 # Otherwise copy from /.home/.ssh if available
 elif [ -f  /.home/.ssh/id_rsa ]; then
   cp /.home/.ssh/id_rsa* ~/.ssh/
