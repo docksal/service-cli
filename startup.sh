ca3aa0f7f002c4d31a4de05244f2c54940b8c4e0
Leonid Makarov <leonid.makarov@ffwagency.com>
2015-09-05 00:09:32 -0400

    chmod 600 ssh key, support for new mapping in b2d: /.ssh-b2d => /.home/.ssh

--- startup.sh
+++ startup.sh
@@ -4,9 +4,11 @@
 # First see if we have a mount at /.ssh
 if [ -f  /.ssh/id_rsa ]; then
   cp /.ssh/id_rsa* ~/.ssh/
-# Otherwise copy from /.ssh-b2d if available
-elif [ -f  /.ssh-b2d/id_rsa ]; then
-  cp /.ssh-b2d/id_rsa* ~/.ssh/
+  chmod 600 ~/.ssh/id_rsa*
+# Otherwise copy from /.home/.ssh if available
+elif [ -f  /.home/.ssh/id_rsa ]; then
+  cp /.home/.ssh/id_rsa* ~/.ssh/
+  chmod 600 ~/.ssh/id_rsa*
 fi
 
 echo "PHP5-FPM with environment variables"
