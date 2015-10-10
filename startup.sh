d17941c047b2bb690c7a7da6f611a0b0abf2e662
Leonid Makarov <leonid.makarov@ffwagency.com>
2015-10-09 20:50:47 -0400

    Wrap php-fpm variables with single quotes to avoid issues with special characters

--- startup.sh
+++ startup.sh
@@ -58,14 +58,14 @@ echo "PHP5-FPM with environment variables"
 # Update php5-fpm with access to Docker environment variables
 ENV_CONF=/etc/php5/fpm/pool.d/env.conf
 echo '[www]' > $ENV_CONF
-for var in $(env | awk -F= '{print $1}'); do
-	# Skip empty/bad variables as this will blow up PHP FPM.
-	if [[ ${!var} == '' || ${var} == '_' ]]; then
-		echo "Skipping empty/bad variable: ${var}"
-	else
-		echo "Adding variable: ${var} = ${!var}"
-		echo "env[${var}] = ${!var}" >> $ENV_CONF
-	fi
+for var in $(env | awk -F = '{print $1}'); do
+  # Skip empty/bad variables as this will blow up PHP FPM.
+  if [[ ${!var} == '' || ${var} == '_' ]]; then
+    echo "Skipping empty/bad variable: '"${var}"'"
+  else
+    echo "Adding variable: '"${var}"' = '"${!var}"'"
+    echo "env['"${var}"'] = '"${!var}"'" >> $ENV_CONF
+  fi
 done
 
 # Execute passed CMD arguments
