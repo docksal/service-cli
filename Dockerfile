docksal/service-cli

commit 5d7abec9eb27262f9ed14aa28185cfcf107bc5c7

Normalize sed

Dockerfile
@@ -69,20 +69,20 @@ RUN DEBIAN_FRONTEND=noninteractive apt-get update && \
 ## PHP settings
 RUN mkdir -p /var/www/docroot && \
     # PHP-FPM settings
-    sed -i 's/memory_limit = .*/memory_limit = 256M/' /etc/php5/fpm/php.ini && \
-    sed -i 's/max_execution_time = .*/max_execution_time = 300/' /etc/php5/fpm/php.ini && \
-    sed -i 's/upload_max_filesize = .*/upload_max_filesize = 500M/' /etc/php5/fpm/php.ini && \
-    sed -i 's/post_max_size = .*/post_max_size = 500M/' /etc/php5/fpm/php.ini && \
+    sed -i '/memory_limit = /c memory_limit = 256M/' /etc/php5/fpm/php.ini && \
+    sed -i '/max_execution_time = /c max_execution_time = 300/' /etc/php5/fpm/php.ini && \
+    sed -i '/upload_max_filesize = /c upload_max_filesize = 500M/' /etc/php5/fpm/php.ini && \
+    sed -i '/post_max_size = /c post_max_size = 500M/' /etc/php5/fpm/php.ini && \
     sed -i '/error_log = php_errors.log/c error_log = \/dev\/stdout/' /etc/php5/fpm/php.ini && \
     sed -i '/;always_populate_raw_post_data/c always_populate_raw_post_data = -1/' /etc/php5/fpm/php.ini
     sed -i '/user = /c user = docker' /etc/php5/fpm/pool.d/www.conf && \
     sed -i '/listen = /c listen = 0.0.0.0:9000' /etc/php5/fpm/pool.d/www.conf && \
     sed -i '/listen.allowed_clients/c ;listen.allowed_clients =' /etc/php5/fpm/pool.d/www.conf && \
-    sed -i '/;daemonize = yes/c daemonize = no' /etc/php5/fpm/php-fpm.conf && \
+    sed -i '/;daemonize/c daemonize = no' /etc/php5/fpm/php-fpm.conf && \
     sed -i '/;catch_workers_output/c catch_workers_output = yes' /etc/php5/fpm/php-fpm.conf && \
     # PHP CLI settings
-    sed -i 's/memory_limit = .*/memory_limit = 512M/' /etc/php5/cli/php.ini && \
-    sed -i 's/max_execution_time = .*/max_execution_time = 600/' /etc/php5/cli/php.ini && \
+    sed -i '/memory_limit = /c memory_limit = 512M/' /etc/php5/cli/php.ini && \
+    sed -i '/max_execution_time = /c max_execution_time = 600/' /etc/php5/cli/php.ini && \
     sed -i '/error_log = php_errors.log/c error_log = \/dev\/stdout/' /etc/php5/cli/php.ini && \
     # PHP module settings
     echo 'opcache.memory_consumption=128' >> /etc/php5/mods-available/opcache.ini

commit ff75c86eacf2222a405018989ea632cad4f6b56f

Remove trailing slashes in php configs
Fixes regression introduced in 5d7abec

Dockerfile
@@ -75,21 +75,21 @@ RUN DEBIAN_FRONTEND=noninteractive apt-get update && \
 ## PHP settings
 RUN mkdir -p /var/www/docroot && \
     # PHP-FPM settings
-    sed -i '/memory_limit = /c memory_limit = 256M/' /etc/php5/fpm/php.ini && \
-    sed -i '/max_execution_time = /c max_execution_time = 300/' /etc/php5/fpm/php.ini && \
-    sed -i '/upload_max_filesize = /c upload_max_filesize = 500M/' /etc/php5/fpm/php.ini && \
-    sed -i '/post_max_size = /c post_max_size = 500M/' /etc/php5/fpm/php.ini && \
-    sed -i '/error_log = php_errors.log/c error_log = \/dev\/stdout/' /etc/php5/fpm/php.ini && \
-    sed -i '/;always_populate_raw_post_data/c always_populate_raw_post_data = -1/' /etc/php5/fpm/php.ini
+    sed -i '/memory_limit = /c memory_limit = 256M' /etc/php5/fpm/php.ini && \
+    sed -i '/max_execution_time = /c max_execution_time = 300' /etc/php5/fpm/php.ini && \
+    sed -i '/upload_max_filesize = /c upload_max_filesize = 500M' /etc/php5/fpm/php.ini && \
+    sed -i '/post_max_size = /c post_max_size = 500M' /etc/php5/fpm/php.ini && \
+    sed -i '/error_log = php_errors.log/c error_log = \/dev\/stdout' /etc/php5/fpm/php.ini && \
+    sed -i '/;always_populate_raw_post_data/c always_populate_raw_post_data = -1' /etc/php5/fpm/php.ini && \
     sed -i '/user = /c user = docker' /etc/php5/fpm/pool.d/www.conf && \
     sed -i '/listen = /c listen = 0.0.0.0:9000' /etc/php5/fpm/pool.d/www.conf && \
     sed -i '/listen.allowed_clients/c ;listen.allowed_clients =' /etc/php5/fpm/pool.d/www.conf && \
     sed -i '/;daemonize/c daemonize = no' /etc/php5/fpm/php-fpm.conf && \
     sed -i '/;catch_workers_output/c catch_workers_output = yes' /etc/php5/fpm/php-fpm.conf && \
     # PHP CLI settings
-    sed -i '/memory_limit = /c memory_limit = 512M/' /etc/php5/cli/php.ini && \
-    sed -i '/max_execution_time = /c max_execution_time = 600/' /etc/php5/cli/php.ini && \
-    sed -i '/error_log = php_errors.log/c error_log = \/dev\/stdout/' /etc/php5/cli/php.ini && \
+    sed -i '/memory_limit = /c memory_limit = 512M' /etc/php5/cli/php.ini && \
+    sed -i '/max_execution_time = /c max_execution_time = 600' /etc/php5/cli/php.ini && \
+    sed -i '/error_log = php_errors.log/c error_log = \/dev\/stdout' /etc/php5/cli/php.ini && \
     # PHP module settings
     echo 'opcache.memory_consumption=128' >> /etc/php5/mods-available/opcache.ini
 