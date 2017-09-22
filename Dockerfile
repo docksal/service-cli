docksal/service-cli

commit 21a452a321bf7556848e76c3d44d50faaf2356ba

Misc cleanup

Dockerfile
@@ -111,7 +111,7 @@ RUN DEBIAN_FRONTEND=noninteractive apt-get update && \
     rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
 ## PHP settings
-RUN mkdir -p /var/www/docroot && \
+RUN \
     # PHP-FPM settings
     ## /etc/php5/fpm/php.ini
     sed -i '/memory_limit =/c memory_limit = 256M' /etc/php5/fpm/php.ini && \
@@ -256,30 +256,31 @@ COPY config/supervisord.conf /etc/supervisor/conf.d/supervisord.conf
 COPY startup.sh /opt/startup.sh
 COPY healthcheck.sh /opt/healthcheck.sh
 
-HEALTHCHECK --interval=5s --timeout=1s CMD ["/opt/healthcheck.sh"]
+ENV \
+	# ssh-agent proxy socket (requires docksal/ssh-agent)
+	SSH_AUTH_SOCK=/.ssh-agent/proxy-socket \
+	# Set TERM so text editors/etc. can be used
+	TERM=xterm \
+	# Allow PROJECT_ROOT to be universally used in fin custom commands (inside and outside cli)
+	PROJECT_ROOT=/var/www
+
+USER root
 
 EXPOSE 9000
 EXPOSE 22
 
 WORKDIR /var/www
 
-ENV \
-	# Default SSH key name
-	SSH_KEY_NAME=id_rsa \
-	# ssh-agent proxy socket (requires docksal/ssh-agent)
-	SSH_AUTH_SOCK=/.ssh-agent/proxy-socket \
-	# Set TERM so text editors/etc. can be used
-	TERM=xterm \
-	# Allow PROJECT_ROOT to be universally used in fin custom commands (inside and outside cli)
-	PROJECT_ROOT=/var/www \
-	# Allow matching host uid:gid
-	HOST_UID=1000 \
-	HOST_GID=100
-
-USER root
-
 # Starter script
 ENTRYPOINT ["/opt/startup.sh"]
 
 # By default, launch supervisord to keep the container running.
 CMD ["supervisord"]
+
+# Health check script
+HEALTHCHECK --interval=5s --timeout=1s --retries=12 CMD ["/opt/healthcheck.sh"]
+
+# Keeping the volumes at the bottom as they would otherwise slow down each build step after them.
+# We want data in this folder to be preserved when container is recreated (configuration updated).
+VOLUME /home/docker
+VOLUME /var/www

startup.sh
@@ -29,14 +29,10 @@ xdebug_enable()
 }
 
 # Docker user uid/gid mapping to the host user uid/gid
-# '""' is used as an empty variable designation in yml files (can't used empty vars without warnings from compose)
-# TODO: figure out a better way of checking for empty variables
-( [[ "$HOST_UID" != '' ]] || [[ "$HOST_UID" != '""' ]] ) &&
-	( [[ "$HOST_GID" != '' ]] || [[ "$HOST_GID" != '""' ]] ) &&
-	uid_gid_reset
+[[ "$HOST_UID" != "" ]] && [[ "$HOST_GID" != "" ]] && uid_gid_reset
 
 # Enable xdebug
-[[ "$XDEBUG_ENABLED" != "0" ]] && xdebug_enable
+[[ "$XDEBUG_ENABLED" != "" ]] && [[ "$XDEBUG_ENABLED" != "0" ]] && xdebug_enable
 
 # Make sure permissions are correct (after uid/gid change and COPY operations in Dockerfile)
 # To not bloat the image size permissions on the home folder are reset during image startup (in startup.sh)

commit 454041902dcfeec5b10004ca06b31a72050cd3da

Added default HOST_UID and HOST_GID values back.
Fixes a regression introduced in 21a452a. Closes #11

Dockerfile
@@ -262,7 +262,10 @@ ENV \
 	# Set TERM so text editors/etc. can be used
 	TERM=xterm \
 	# Allow PROJECT_ROOT to be universally used in fin custom commands (inside and outside cli)
-	PROJECT_ROOT=/var/www
+	PROJECT_ROOT=/var/www \
+	# Default values for HOST_UID and HOST_GUI to match the default Ubuntu user. These are used in startup.sh
+	HOST_UID=1000 \
+	HOST_GID=1000
 
 USER root
 