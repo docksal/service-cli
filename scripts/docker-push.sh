75851d243973d429f0e5f5f63008a8c4ac52fdbc
Sergey Zabolotny <sergey.zabolotny@ffwagency.com>
2019-11-20 16:08:54 +0200

    Update makefile and deploy script

--- scripts/docker-push.sh
+++ scripts/docker-push.sh
@@ -40,27 +40,23 @@ tag_and_push ()
 	echo "Pushing ${target} image ..."
 	docker tag ${source} ${target}
 	docker push ${target}
-
-	# Cloud9 flavor
-	echo "Pushing ${target}-ide image ..."
-	docker tag ${source}-ide ${target}-ide
-	docker push ${target}-ide
 }
 
 # ---------------------------- #
 
 # Possible docker image tags
-IMAGE_TAG_EDGE="edge-php${VERSION}"
-IMAGE_TAG_STABLE="php${VERSION}"
+IMAGE_TAG_EDGE="edge${APPENDIX:+-${APPENDIX}}"
+IMAGE_TAG_STABLE="stable${APPENDIX:+-${APPENDIX}}"
 
 # Read the split parts
 IFS='.' read -a ver_arr <<< "$TRAVIS_TAG"
 
-# Major version, e.g. 2-php7.2
-IMAGE_TAG_RELEASE_MAJOR="${ver_arr[0]#v*}-php${VERSION}"
+# Major version, e.g. 2[-APPENDIX]
+# APPENDIX may be php version php7.3 for example
+IMAGE_TAG_RELEASE_MAJOR="${ver_arr[0]#v*}${APPENDIX:+-${APPENDIX}}"
 
-# Major-minor version, e.g. 2.5-php7.2
-IMAGE_TAG_RELEASE_MAJOR_MINOR="${ver_arr[0]#v*}.${ver_arr[1]}-php${VERSION}"
+# Major-minor version, e.g. 2.7[-APPENDIX]
+IMAGE_TAG_RELEASE_MAJOR_MINOR="${ver_arr[0]#v*}.${ver_arr[1]}${APPENDIX:+-${APPENDIX}}"
 IMAGE_TAG_LATEST="latest"
 
 # Skip pull request builds
@@ -70,14 +66,14 @@ docker login -u "${DOCKER_USER}" -p "${DOCKER_PASS}"
 
 # Push images
 if is_edge; then
-	tag_and_push ${REPO}:build-${VERSION} ${REPO}:${IMAGE_TAG_EDGE} # Example tag: edge-php7.3
+	tag_and_push ${REPO}:${TAG} ${REPO}:${IMAGE_TAG_EDGE} # Example tag: edge[-APPENDIX]
 elif is_stable; then
-	tag_and_push ${REPO}:build-${VERSION} ${REPO}:${IMAGE_TAG_STABLE} # Example tag: php7.3
+	tag_and_push ${REPO}:${TAG} ${REPO}:${IMAGE_TAG_STABLE} # Example tag: stable[-APPENDIX]
 elif is_release; then
 	# Have stable, major, minor tags match
-	tag_and_push ${REPO}:build-${VERSION} ${REPO}:${IMAGE_TAG_STABLE} # Example tag: php7.3
-	tag_and_push ${REPO}:build-${VERSION} ${REPO}:${IMAGE_TAG_RELEASE_MAJOR}  # Example tag: 2-php7.3
-	tag_and_push ${REPO}:build-${VERSION} ${REPO}:${IMAGE_TAG_RELEASE_MAJOR_MINOR}  # Example tag: 2.7-php7.3
+	tag_and_push ${REPO}:${TAG} ${REPO}:${IMAGE_TAG_STABLE} # Example tag: stable[-APPENDIX]
+	tag_and_push ${REPO}:${TAG} ${REPO}:${IMAGE_TAG_RELEASE_MAJOR}  # Example tag: 2[-APPENDIX]
+	tag_and_push ${REPO}:${TAG} ${REPO}:${IMAGE_TAG_RELEASE_MAJOR_MINOR}  # Example tag: 2.7[-APPENDIX]
 else
 	# Exit if not on develop, master or release tag
 	exit
@@ -86,7 +82,5 @@ fi
 # Special case for the "latest" tag
 # Push (base image only) on stable and release builds
 if is_latest && (is_stable || is_release); then
-	echo "Pushing ${REPO}:${IMAGE_TAG_LATEST} image ..."
-	docker tag ${REPO}:build-${VERSION} ${REPO}:${IMAGE_TAG_LATEST}
-	docker push ${REPO}:${IMAGE_TAG_LATEST}
+	tag_and_push ${REPO}:${TAG} ${REPO}:${IMAGE_TAG_LATEST}
 fi
