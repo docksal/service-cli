7abab41fb68963e8cec65417be9793d1ae6729d9
Leonid Makarov <leonid.makarov@wesalute.com>
2026-02-02 13:54:03 +0100

    Added opcache.preload_user directive in opcache.ini
    Fixes PHP 8.5 crashing with 'Error "opcache.preload" requires "opcache.preload_user" when running under uid 0'

--- tests/.docksal/etc/php/preload.php
+++ tests/.docksal/etc/php/preload.php
@@ -1,2 +1,4 @@
 <?php
-
+// Preload script for PHP CLI container.
+// This file is loaded before any other PHP code when the container starts. 
+// See https://www.php.net/manual/en/opcache.preloading.php
